6b0e675d92b3f36048b5138cc5fdc8aa1495be71
roozbehamidi <106980595+roozbehamidi@users.noreply.github.com>
2025-04-25 00:34:23 +0430

    fix: move engine init to FastAPI startup event

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from app.routers import auth, todo
 import uvicorn
 from app.database import init_engine
 
-init_engine()
 app = FastAPI(title="TODO API")
+
+@app.on_event("startup")
+async def startup_event():
+    init_engine()
 
 app.include_router(auth.router)
 app.include_router(todo.router)
 
-#if __name__ == "__main__":
-
-#uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
